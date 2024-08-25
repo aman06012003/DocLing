6585c7f4ca902202b0d33c6848073f3792c719e5
Aman Kapoor <akalasklo@gmail.com>
2024-08-25 19:59:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ def change_bgm():
 change_bgm()
 
 # Initialize the Cohere API key
-os.environ["COHERE_API_KEY"] = "hEfcl4uOqse4sCyqEnmQ1Q5OnI9UzdO8e9rSHYL3"
+os.environ["COHERE_API_KEY"] = "XyBudfarv1bxPBBJRDK6BpX00NujnPz3RTRFXioy"
 
 st.title("Doclingual - Multilingual Document Understanding")
 
